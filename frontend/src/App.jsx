4b3a72a536756cfef2d3fcdd923eea7669163765
Mathew M <mathewmastromonaco@gmail.com>
2024-08-19 03:12:08 -0400

    Incorporate nav bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
-
+import TopNavigation from 'components/TopNavigationBar';
 import PhotoList from './components/PhotoList';
-import PhotoListItem from './components/PhotoListItem';
-import TopicList from 'components/TopicList';
-import TopicListItem from 'components/TopicListItem';
 import './App.scss';
+
 
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-    <TopicList/>
+    <TopNavigation/>
     <PhotoList/>
     </div>
   );
