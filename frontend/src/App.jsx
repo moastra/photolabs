069f816084849632da9d6ae687abc4403782354c
Mathew M <mathewmastromonaco@gmail.com>
2024-08-18 23:15:36 -0400

    create PhotoListItem component and implement it with props

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 
+import PhotoList from './components/PhotoList';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
+
+const sampleDataForPhotoListItem = {
+  id: "1",
+  location: {
+    city: "Montreal",
+    country: "Canada",
+  },
+  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  username: "Joe Example",
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+};
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <PhotoListItem/>
+      <PhotoListItem
+        imageSource={sampleDataForPhotoListItem.imageSource}
+        profile={sampleDataForPhotoListItem.profile}
+        username={sampleDataForPhotoListItem.username}
+        location={sampleDataForPhotoListItem.location}
+        />
     </div>
   );
 };
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,19 +3,17 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = ({ imageSource, profile, username, location}) => {
+  return (
+    <div className="photoListItem">
+      <img src={imageSource} alt="Photo" className="photo-image" />
+      <img src={profile} alt="Profile" className="profile-image" />
+      <div className="username">{username}</div>
+      <div className="photo-location">
+        {location.city}, {location.country}
+      </div>
+    </div>
+  );
 };
 
 export default PhotoListItem;
