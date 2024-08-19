d4f2d589efd6d782cf4a8e1db0f4aa20cfa26f92
Mathew M <mathewmastromonaco@gmail.com>
2024-08-18 23:33:57 -0400

    Add 2 more sampleData in order to display 3 images

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,8 @@ import PhotoList from './components/PhotoList';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
-const sampleDataForPhotoListItem = {
+const sampleDataForPhotoListItem = [
+  {
   id: "1",
   location: {
     city: "Montreal",
@@ -13,18 +14,42 @@ const sampleDataForPhotoListItem = {
   imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
   username: "Joe Example",
   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
+},
+{
+  id: "2",
+  location: {
+    city: "Montreal",
+    country: "Canada",
+  },
+  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  username: "Joe Example",
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+},
+{
+  id: "3",
+  location: {
+    city: "Montreal",
+    country: "Canada",
+  },
+  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  username: "Joe Example",
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+}
+];
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <PhotoListItem
-        imageSource={sampleDataForPhotoListItem.imageSource}
-        profile={sampleDataForPhotoListItem.profile}
-        username={sampleDataForPhotoListItem.username}
-        location={sampleDataForPhotoListItem.location}
+      {sampleDataForPhotoListItem.map((photo) => (
+        <PhotoListItem
+        key={photo.id}
+        imageSource={photo.imageSource}
+        profile={photo.profile}
+        username={photo.username}
+        location={photo.location}
         />
+      ))}
     </div>
   );
 };
